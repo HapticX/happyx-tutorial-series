895dd3f9315787e635640c954dfaafea266f26b6
Ethosa <social.ethosa@gmail.com>
2023-05-24 12:27:18 +0700

    new article https://dev.to/ethosa/writing-simple-restapi-in-nim-with-happyx-2-3hof

--- simple_rest_api/src/main.nim
+++ simple_rest_api/src/main.nim
@@ -5,8 +5,36 @@ import
 
 # Setting up our server at `127.0.0.1:5000`
 serve("127.0.0.1", 5000):
+  var posts = %*[
+    {
+      "title": "Hello",
+      "text": "world"
+    }, {
+      "title": "Bye",
+      "text": ":("
+    }, {
+      "title": "Perfect nim web framework?",
+      "text": "it's HappyX 🍍"
+    },
+  ]
 
   # on GET HTTP method at 127.0.0.1:5000/hello-world
   get "/hello-world":
     # We'll see "Hello, world!"
     "Hello, world!"
+  
+  # on GET HTTP method at 127.0.0.1:5000/posts
+  get "/posts":
+    # will responds all posts
+    req.answerJson(posts)
+  
+  # on GET HTTP method at 127.0.0.1:5000/post
+  get "/post$index:int":  # index is post index
+    if posts.len > index:
+      # try open 127.0.0.1:5000/post0
+      req.answerJson(posts[index])
+    else:
+      # try open 127.0.0.1:5000/post10
+      req.answerJson({
+        "error": fmt"post index is wrong"
+      })
