91224af9718e44ce321a24298eca594c9b38046d
Ethosa <social.ethosa@gmail.com>
2023-06-24 07:15:14 +0700

    update simple_rest_api

--- simple_rest_api/src/main.nim
+++ simple_rest_api/src/main.nim
@@ -1,13 +1,7 @@
 import
   # This will import HappyX framework
-  happyx
-
-
-# Request model AddPost
-# with two string fields
-model AddPost:
-  title: string
-  text: string
+  happyx,
+  ./mounts/[posts]
 
 
 # Setting up our server at `127.0.0.1:5000`
@@ -25,33 +19,9 @@ serve("127.0.0.1", 5000):
     },
   ]
 
+  mount "/posts" -> Posts
+
   # on GET HTTP method at 127.0.0.1:5000/hello-world
   get "/hello-world":
     # We'll see "Hello, world!"
     "Hello, world!"
-  
-  # on GET HTTP method at 127.0.0.1:5000/posts
-  get "/posts":
-    # will responds all posts
-    return posts
-  
-  # on GET HTTP method at 127.0.0.1:5000/post
-  get "/post$index:int":  # index is post index
-    if posts.len > index:
-      # try open 127.0.0.1:5000/post0
-      return posts[index]
-    else:
-      # try open 127.0.0.1:5000/post10
-      return {
-        "error": "post index is wrong"
-      }
-  
-  # on POST HTTP method at 127.0.0.1:5000/post/
-  post "/post/[postData:AddPost]":
-    posts.add(%*{
-      "title": postData.title,
-      "text": postData.text
-    })
-    return {"response": {
-      "index": posts.len - 1
-    }}
---
+++ simple_rest_api/src/mounts/posts.nim
@@ -0,0 +1,36 @@
+import happyx
+
+
+# Request model AddPost
+# with two string fields
+model AddPost:
+  title: string
+  text: string
+
+
+mount Posts:
+  # on GET HTTP method at 127.0.0.1:5000/posts
+  get "/":
+    # will responds all posts
+    return posts
+  
+  # on GET HTTP method at 127.0.0.1:5000/post
+  get "/$index:int":  # index is post index
+    if posts.len > index:
+      # try open 127.0.0.1:5000/post0
+      return posts[index]
+    else:
+      # try open 127.0.0.1:5000/post10
+      return {
+        "error": "post index is wrong"
+      }
+  
+  # on POST HTTP method at 127.0.0.1:5000/post/
+  post "/[postData:AddPost]":
+    posts.add(%*{
+      "title": postData.title,
+      "text": postData.text
+    })
+    return {"response": {
+      "index": posts.len - 1
+    }}
